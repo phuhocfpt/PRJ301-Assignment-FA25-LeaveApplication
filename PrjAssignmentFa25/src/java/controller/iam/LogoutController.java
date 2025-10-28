f711feb498b3d072c0bff425db0a6423c02d26d1
phuhocfpt <phulearnse@gmail.com>
2025-10-28 21:41:05 +0700

    create LogoutController
    Tạo ra LogoutController chức năng tương tự LoginController. Sau khi /logout thì sẽ trả về trang login

--- PrjAssignmentFa25/src/java/controller/iam/LogoutController.java
+++ PrjAssignmentFa25/src/java/controller/iam/LogoutController.java
@@ -4,10 +4,41 @@
  */
 package controller.iam;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.iam.User;
+
 /**
  *
  * @author phuga
  */
-public class LogoutController {
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends BaseRequiredAuthenticationController{
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, User user)
+            throws ServletException, IOException {
+        // 1. Lấy session hiện tại (đã có từ đăng nhập)
+        HttpSession session = req.getSession();
+        
+        // 2. Hủy bỏ session (xóa attribute "acc" và các attribute khác)
+        session.invalidate();
+        
+        // 3. Chuyển hướng người dùng về trang đăng nhập
+        resp.sendRedirect(req.getContextPath() + "/login");
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, User user)
+            throws ServletException, IOException {
+        doGet(req, resp, user); // Gọi lại hàm doGet ở trên để xử lý
+    }
+    
+
     
 }
